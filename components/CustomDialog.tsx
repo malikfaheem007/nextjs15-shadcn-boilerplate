caac34ba2e13582f930b961ce83cb744b9adeeb2
Usama Ali <malikusamaali6370@gmail.com>
2025-04-17 12:07:31 +0500

    add CustomDialog

---
+++ components/CustomDialog.tsx
@@ -0,0 +1,67 @@
+import { cn } from "@/lib/utils";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "./ui/dialog";
+import { Button } from "./ui/button";
+import { LoadingButton } from "./LoadingButton";
+
+interface CustomDialogProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  title: string;
+  description?: string;
+  isLoading?: boolean;
+  secondaryBtnText?: string;
+  primaryBtnText?: string;
+  formId?: string;
+  children: React.ReactNode;
+  className?: string;
+}
+
+export function CustomDialog({
+  children,
+  title,
+  description,
+  open,
+  onOpenChange,
+  formId,
+  secondaryBtnText = "Cancel",
+  primaryBtnText = "Save",
+  isLoading,
+  className,
+}: CustomDialogProps) {
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent
+        className={cn("flex max-h-[80vh] p-0 flex-col", className)}
+      >
+        <div className="flex min-h-0 flex-1 flex-col">
+          <DialogHeader className="border-b px-6 py-4">
+            <DialogTitle>{title}</DialogTitle>
+            {description && (
+              <DialogDescription>{description}</DialogDescription>
+            )}
+          </DialogHeader>
+          <div className="flex-1 overflow-y-auto px-6 py-4">{children}</div>
+          <DialogFooter className="border-t px-6 py-4">
+            <Button
+              type="button"
+              onClick={() => onOpenChange?.(false)}
+              variant="outline"
+            >
+              {secondaryBtnText}
+            </Button>
+            <LoadingButton type="submit" form={formId} disabled={isLoading}>
+              {primaryBtnText}
+            </LoadingButton>
+          </DialogFooter>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
